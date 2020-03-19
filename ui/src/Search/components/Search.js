eda7ac6e93909bd5f217c649b17b9d070a2cdfa0
Chris Chase <cchase@redhat.com>
2020-03-19 10:54:36 -0400

    WIP - frontend updates

--- ui/src/Search/components/Search.js
+++ ui/src/Search/components/Search.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect, useCallback } from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect, useCallback } from 'react';
+import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { resetSearch, searchPhoto } from "../actions";
+import { resetSearch, searchPhoto } from '../actions';
 import Camera from '@material-ui/icons/Camera';
 import Cached from '@material-ui/icons/Cached';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
-import "./Search.scss";
+import './Search.scss';
+
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -16,6 +18,45 @@ const useStyles = makeStyles(theme => ({
     display: 'none',
   },
 }));
+
+const labelSettings = {
+  'Dynatrace': {
+    bgColor: '#3DB048',
+    width: 107
+  },
+  'IBM': {
+    bgColor: '#1870C2',
+    width: 42
+  },
+  'Intel': {
+    bgColor: '#0071C6',
+    width: 49
+  },
+  'Anaconda': {
+    bgColor: '#3DB048',
+    width: 110
+  },
+  'SAS': {
+    bgColor: '#007CC2',
+    width: 60
+  },
+  'Cloudera': {
+    bgColor: '#F96703',
+    width: 110
+  },
+  'Red Hat': {
+    bgColor: '#EE0001',
+    width: 110
+  }
+};
+
+function getLabelSettings(label) {
+  const defaultSettings = {
+    color: '#EE0001'
+  };
+
+  return (labelSettings[label] || defaultSettings)
+}
 
 function Search(
   {
@@ -28,9 +69,11 @@ function Search(
   }) {
   const [image, setImage] = useState(null);
   const [cameraEnabled, setCameraEnabled] = useState(null);
+  const [cameraOverlay, setCameraOverlay] = useState(null);
   const [video, setVideo] = useState(null);
-  const [canvas, setCanvas] = useState(null);
-  const [facingMode, setFacingMode] = useState("user");
+  const [imageCanvas, setImageCanvas] = useState(null);
+  const [imageOverlay, setImageOverlay] = useState(null);
+  const [facingMode, setFacingMode] = useState('user');
 
   const classes = useStyles();
 
@@ -39,7 +82,7 @@ function Search(
   }, []);
 
   useEffect(() => {
-    drawBoxes();
+    drawDetections();
   }, [inference]);
 
   const videoRef = useCallback(node => {
@@ -50,8 +93,8 @@ function Search(
     }
   }, []);
 
-  const canvasRef = useCallback(node => {
-    setCanvas(node);
+  const imageCanvasRef = useCallback(node => {
+    setImageCanvas(node);
   }, []);
 
   function enableCamera() {
@@ -65,10 +108,10 @@ function Search(
   }
 
   function onCameraClicked() {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    imageCanvas.width = video.videoWidth;
+    imageCanvas.height = video.videoHeight;
 
-    canvas.getContext('2d').drawImage(
+    imageCanvas.getContext('2d').drawImage(
       video,
       0, 0,
       video.videoWidth,
@@ -79,40 +122,56 @@ function Search(
       track.stop();
     });
 
-    setImage(canvas.toDataURL());
+    setImage(imageCanvas.toDataURL());
     setCameraEnabled(false);
 
-    let imageData = canvas.toDataURL('image/jpeg');
+    let imageData = imageCanvas.toDataURL('image/jpeg');
     searchPhoto(imageData);
   }
 
-  function drawBoxes() {
-    if (!inference || !canvas.getContext) {
+  function drawDetections() {
+    if (!inference || !imageCanvas.getContext) {
       return
     }
 
-    inference.detections.forEach(d => drawBox(d));
+    inference.detections.forEach(d => drawDetection(d));
   }
 
-  function drawBox({box, label, score}) {
-    const ctx = canvas.getContext('2d');
-    const width = Math.floor((box.xMax - box.xMin) * canvas.width);
-    const height = Math.floor((box.yMax - box.yMin) * canvas.height);
-    const x = Math.floor(box.xMin * canvas.width);
-    const y = Math.floor(box.yMin * canvas.height);
+  function drawDetection({box, label, score}) {
+    // const ctx = imageCanvas.getContext('2d');
+    const width = Math.floor((box.xMax - box.xMin) * imageCanvas.width);
+    const height = Math.floor((box.yMax - box.yMin) * imageCanvas.height);
+    const x = Math.floor(box.xMin * imageCanvas.width);
+    const y = Math.floor(box.yMin * imageCanvas.height);
+    const labelSettings = getLabelSettings(label);
+    drawBox(x, y, width, height, labelSettings.bgColor);
+    drawBoxTextBG(x + 10, y + height - 33, labelSettings.width, 30, labelSettings.bgColor);
+    drawBoxText(label, x + 10, y + height - 10)
+  }
+
+  function drawBox(x, y, width, height, color) {
+    const ctx = imageCanvas.getContext('2d');
     ctx.lineWidth = 3;
-
-    if (score > .9) {
-      ctx.strokeStyle = "lightgreen";
-    } else if (score > .8) {
-      ctx.strokeStyle = "yellow";
-    } else {
-      ctx.strokeStyle = "orange";
-    }
-
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = color;
+    // ctx.setLineDash([16, 16]);
     ctx.strokeRect(x, y, width, height);
-    ctx.font = '32px sans-serif';
-    ctx.strokeText(label, x + 10, y + height - 10);
+  }
+
+  function drawBoxTextBG(x, y, width, height, color) {
+    const ctx = imageCanvas.getContext('2d');
+
+    // ctx.strokeStyle = getLabelSettings(label).color;
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, 30);
+  }
+
+  function drawBoxText(text, x, y) {
+    const ctx = imageCanvas.getContext('2d');
+    // ctx.lineWidth = 0;
+    ctx.font = '24px Roboto';
+    ctx.fillStyle = 'white';
+    ctx.fillText(text, x, y);
   }
 
   function renderCamera() {
@@ -121,24 +180,26 @@ function Search(
     }
 
     return (
-      <div className="camera">
-        <div className="img-container">
+      <div className='camera'>
+        <div className='img-container'>
           <video
-            className="camera-preview"
+            className='camera-preview'
             ref={videoRef}
             controls={false}
             autoPlay
             playsInline
           />
+          <div className='horizontal camera-overlay'></div>
+          <div className='vertical camera-overlay'></div>
         </div>
-        <div className="action-container">
-          <div className="button-bar">
+        <div className='action-container'>
+          <div className='button-bar'>
             <Button
-              variant="contained"
-              size="large"
-              className="take-picture-button"
+              variant='contained'
+              size='large'
+              className='take-picture-button'
               onClick={onCameraClicked}
-            ><Camera style={{fontSize: 60}}/>
+            ><RadioButtonUncheckedIcon style={{fontSize: 60}}/>
             </Button>
           </div>
         </div>
@@ -148,54 +209,33 @@ function Search(
 
 
   function renderSnapshot() {
-    const display = image ? "flex" : "none";
-
-    let inferenceResult = null;
-    if (inferencePending) {
-      inferenceResult = (
-        <div className="inference-result in-progress">
-          <div className="inference-result-line">Loading...</div>
-        </div>);
-    } else if (inference) {
-      if (!inference.detections || inference.detections.length === 0) {
-        inferenceResult = (
-          <div className="inference-result no-logos">
-            <div className="inference-result-line">No Logos Found</div>
-          </div>);
-      } else {
-      inferenceResult = (
-        <div className="inference-result logos">
-          {inference.detections.map((d, index) => <div className="inference-result-line" key={index}>{d.label} <span className="result-score">{Math.floor(d.score * 100)}%</span></div>)}
-        </div>);
-      }
-    }
+    const display = image ? 'flex' : 'none';
 
     return (
-      <div className="result" style={{display}}>
-        <div className="img-container">
+      <div className='result' style={{display}}>
+        <div className='img-container'>
           <canvas
-            className="search-result"
-            ref={canvasRef}
+            className='search-result'
+            ref={imageCanvasRef}
           />
         </div>
-        <div className="action-container">
-          <div className="button-bar">
+        <div className='action-container'>
+          <div className='button-bar'>
             <Button
-              variant="contained"
-              size="large"
-              className="re-take-picture-button"
+              variant='contained'
+              size='large'
+              className='re-take-picture-button'
               onClick={onCameraToggled}
-            ><Cached style={{fontSize: 60}}/>
+            >Try again
             </Button>
           </div>
-          {inferenceResult}
         </div>
       </div>
     );
   }
 
   return (
-    <div className="search">
+    <div className='search'>
       {renderCamera()}
       {renderSnapshot()}
     </div>
@@ -218,3 +258,4 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
+
